97dea982593438ead8699449eed0d43ca5171e0f
Jstar-s <ming19752@gmail.com>
2021-12-04 19:46:52 +0800

    TableBean ddd

---
+++ src/main/java/com/atguigu/mapreduce/ReduceJoin/TableBean.java
@@ -0,0 +1,31 @@
+package com.atguigu.mapreduce.ReduceJoin;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * @program: MapReduceDemo
+ * @description:
+ * @author: zjh
+ * @create: 2021-12-04 11:02
+ **/
+
+public class TableBean implements Writable {
+
+    private String id;
+    private 
+
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+
+    }
+}
